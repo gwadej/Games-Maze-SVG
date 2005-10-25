062728f2338fa349adde6e2bd09c37bf05173878
gwadej <gwadej@ed3f67d0-e5d7-0310-aef1-da7078c8542f>
2005-10-25 03:27:33 +0000

    Add the scripts I've built to use this module.
    git-svn-id: file:///home/svn/mainrepos/Games-Maze-SVG/trunk@469 ed3f67d0-e5d7-0310-aef1-da7078c8542f

---
+++ bin/MakeMaze.pl
@@ -0,0 +1,108 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use Games::Maze;
+use Games::Maze::Ascii;
+use Games::Maze::SVG;
+use Getopt::Std;
+
+# Parse command line
+my %opts = ();
+
+getopts( 'xXSs:e:n:o:f:b:i', \%opts ) || usage();
+
+usage() if @ARGV == 1;
+
+# Prepare to generate output
+my $build_maze = $opts{S} ? Games::Maze::SVG->new() : Games::Maze::Ascii->new();
+my $out = \*STDOUT;
+
+if($opts{o})
+ {
+  $out = undef;
+  open( $out, ">$opts{o}" ) or die "Unable to create $opts{o}: $!";
+ }
+
+# extract parameters from command line
+my $desc = get_maze_desc( \%opts, @ARGV );
+$build_maze->{mazeparms} = $desc;
+$build_maze->set_wall_form( $opts{f} ) if $opts{f};
+$build_maze->set_interactive() if $opts{i};
+$build_maze->set_crumbstyle( $opts{b} ) if $opts{b};
+
+# build maze
+my $num = $opts{n} || 1;
+print $out $build_maze->toString() while $num-- > 0;
+
+
+# ----------------------------------------
+# Subroutines
+
+# ----------------------
+# Get maze description from the parsed \%opts from the command
+# line and the remaining command line parameters.
+#
+# returns  name of the wall description
+#          the description hash
+sub  get_maze_desc
+ {
+  my $opts     = shift;
+
+  my $defdims  = [18,24,1];
+  my $defxdims = [12,12,1];
+
+  my $dims = $opts->{X} ? $defxdims : $defdims;
+  if(@_)
+   {
+    $dims = [ @_[0..2] ];
+    $dims->[2] ||= 1;
+    $dims->[2] = 1 if $opts->{S}; # SVG mazes are 1 level for now
+   }
+
+  my %desc = ( dimensions => $dims );
+
+  $desc{cell} = 'Hex'     if $opts->{x} || $opts->{X};
+  $desc{form} = 'Hexagon' if $opts->{X};
+  if(defined $opts->{s})
+   {
+    unless($opts->{s} >= 1 and $opts->{s} <= $dims->[0])
+     {
+      die "Starting column out of range.\n";
+     } 
+    $desc{entry} = [ $opts->{s}, 1, 1 ];
+   }
+  if(defined $opts->{e})
+   {
+    unless($opts->{e} >= 1 and $opts->{e} <= $dims->[0])
+     {
+      die "Ending column out of range.\n";
+     } 
+    $desc{exit} = [ $opts->{e}, @{$dims}[1,2] ];
+   }
+
+  (\%desc);
+ }
+
+# ----------------------
+#  Usage message if parameters are messed up.
+sub usage
+ {
+  (my $prog = $0) =~ s/^.\///;
+  print <<EOH;
+Usage: $prog  [-x] [-X] [cols rows [levels]]
+
+where   -x       specifies hexagonal cells
+        -X       specifies a hexagonal maze with hexagonal cells
+	-S       generates an SVG version of the maze
+	-s col   what column holds the entrance
+	-e col   what column holds the exit
+	-f form  wall forms (straight|round|roundcorners|bevel)
+	-n num   how many mazes to print.
+	-o file  write to a file not the screen
+	-i       interactive mode (only for SVG)
+	-b style breadcrumb style (dash|dot|line|none)
+EOH
+  exit 1;
+ }
+
---
+++ bin/svgmaze.pl
@@ -0,0 +1,85 @@
+#!/usr/bin/perl -w
+
+use Games::Maze;
+use Games::Maze::SVG;
+use CGI;
+use strict;
+
+$| = 1;
+
+my $q = CGI::new();
+my %parms = $q->Vars();
+# Fixup for new HTML style
+if($parms{style})
+ {
+  my @pieces = split( ':', $parms{style} );
+  $parms{walls} = $pieces[1];
+  $parms{cell}  = "hex" if $pieces[0] =~ /^[Hh]ex/;
+  $parms{shape} = "hex" if $pieces[0] eq "Hex";
+ }
+
+# Prepare to generate output
+my $build_maze = Games::Maze::SVG::new( dir => '/svg/');
+
+# extract parameters from command line
+my $desc = eval { get_maze_desc( \%parms ); };
+if($@)
+ {
+  my $err = $@;
+  print $q->header, $q->start_html, $q->h1( $err ),
+        $q->p( "Press back button and try again." ),
+        $q->end_html;
+  exit 0;
+ }
+
+$build_maze->{mazeparms} = $desc;
+$build_maze->set_wall_form( $parms{walls} ) if $parms{walls};
+$build_maze->set_interactive();
+$build_maze->set_crumbstyle( $parms{crumb} ) if $parms{crumb};
+
+my $svg = $build_maze->toString();
+print $q->header( -type => "image/svg+xml", -Content_length => length $svg ),
+      $svg;
+
+
+# ----------------------------------------
+# Subroutines
+
+# ----------------------
+# Get maze description from the parsed \%parms from the cgi request
+#
+# returns  the description hash
+sub  get_maze_desc
+ {
+  my $parms    = shift;
+
+  my $defdims  = [18,24,1];
+  my $defxdims = [12,12,1];
+
+  my $dims = $parms->{hex} ? $defxdims : $defdims;
+  $dims->[0] = $parms->{width} if $parms->{width};
+  $dims->[1] = $parms->{height} if $parms->{height};
+
+  my %desc = ( dimensions => $dims );
+
+  $desc{cell} = 'Hex'     if ($parms->{cell}||'') eq "hex";
+  $desc{form} = 'Hexagon' if ($parms->{shape}||'') eq "hex";
+  if($parms->{enter})
+   {
+    unless($parms->{enter} >= 1 and $parms->{enter} <= $dims->[0])
+     {
+      die "Starting column out of range.\n";
+     } 
+    $desc{entry} = [ $parms->{enter}, 1, 1 ];
+   }
+  if($parms->{exit})
+   {
+    unless($parms->{exit} >= 1 and $parms->{exit} <= $dims->[0])
+     {
+      die "Ending column out of range.\n";
+     } 
+    $desc{exit} = [ $parms->{exit}, @{$dims}[1,2] ];
+   }
+
+  (\%desc);
+ }
